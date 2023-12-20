ff4579ffad5a8fd6dbdec098419c6f2ab5e8a533
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-12-20 15:36:45 +0300

    Change bg color on hover

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const container = document.querySelector(".container");
-for (let i=0;i<256;i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement("div");
+  div.classList.add("grid");
+  container.appendChild(div);
+}
+
+const grids = document.querySelectorAll(".grid");
+
+function hoverEffect() {
+  for (grid of grids) {
+    grid.style.background = "gray";
+  }
+}
+
+for (grid of grids) {
+  grid.addEventListener("mouseover", hoverEffect);
 }
