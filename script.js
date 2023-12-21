3f8ae8700050c1c64701e74768dcb706e898692d
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2023-12-21 12:49:01 +0300

    Create reset button

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+//global variables
 const container = document.querySelector(".container");
 const gridButton = document.querySelector("#gridBtn");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid");
-  container.appendChild(div);
+//create grids
+function createGrids() {
+  container.innerHTML = "";
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid");
+    container.appendChild(div);
+  }
 }
 
+createGrids();
 
 const grids = document.querySelectorAll(".grid");
-
 
 function hoverEffect(event) {
   event.target.style.background = "black";
@@ -34,10 +39,12 @@ const changeSquareNumber = () => {
   }
 };
 
-
-
 gridButton.addEventListener("click", changeSquareNumber);
 
 for (box of grids) {
   box.addEventListener("mouseover", hoverEffect);
 }
+
+//reset functionality
+resetBtn = document.querySelector("#resetBtn");
+resetBtn.addEventListener("click", createGrids);
